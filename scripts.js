82467b64b87c2b0dd2a66c6e55044635fd4be866
ISHIKA ISHANI <ishikaishani@ISHIKAs-MacBook-Air.local>
2022-10-13 23:39:56 +0530

    Add Styles

--- scripts.js
+++ scripts.js
@@ -50,18 +50,38 @@
      document.getElementById('todo-list').innerHTML='';
      todos.forEach(function(todo){
          const element= document.createElement('div');
-         element.innerText= todo.title + ' ' + todo.dueDate;
+         element.innerText= todo.title;
+         element.style.display="grid";
+         element.style.gridTemplateColumns="6fr 2fr 4fr";
+         element.style.justifyContent='center';
+         element.style.alignItems='center';
+         
+         element.style.gap='2px';
+         const date= document.createElement('button');
+         date.innerText= todo.dueDate;
+         element.appendChild(date);
          const deleteButton= document.createElement('button');
          deleteButton.innerText = 'Delete';
-         deleteButton.style= 'margin-left:140px'; 
-         deleteButton.style.backgroundColor="blue";
-         deleteButton.style.borderStyle='none';
          deleteButton.style='cursor:pointer';
-         deleteButton.style.color='black';
+         deleteButton.style.color='white';
          deleteButton.onclick=deleteTodo;
          deleteButton.id=todo.id;
          element.appendChild(deleteButton);
          const todoList = document.getElementById('todo-list');
-         todoList.appendChild(element);     
+         todoList.appendChild(element); 
+         deleteButton.style.backgroundColor='rgba(2, 83, 149, 0.979)';
+         deleteButton.style.marginLeft='140px'; 
+         deleteButton.style.borderStyle='none';
+         deleteButton.style.borderRadius='5px';
+         deleteButton.style.width="100px";
+         deleteButton.style.height="30px";
+         date.style.marginLeft='140px'; 
+         date.style.borderStyle='none';
+         date.style.width="100px";
+         date.style.height="30px";
+         date.style.backgroundColor='white';
+         
+         
+             
      });
  }
